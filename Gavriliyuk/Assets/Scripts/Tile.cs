4675bb9621d6d6da574e12b6204aa03e5564d318
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-20 23:39:16 +0300

    strict instantiation
    instantiate at the middle of the tile

--- Gavriliyuk/Assets/Scripts/Tile.cs
+++ Gavriliyuk/Assets/Scripts/Tile.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public GameObject drop;
     private void OnMouseDown()
     {
         Debug.Log("Click");
+        //Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float strictMouseX = Mathf.RoundToInt(mousePos.x);
+        float strictMouseY = Mathf.RoundToInt(mousePos.y);
+        Vector2 newMousePos = new Vector2(strictMouseX, strictMouseY);
+        Instantiate(drop, newMousePos, transform.rotation);
     }
 }
