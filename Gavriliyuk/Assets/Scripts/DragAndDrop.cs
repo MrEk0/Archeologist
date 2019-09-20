9dcfe9b288473798232f418f0e527f28f4ecea2c
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-20 23:36:04 +0300

    drag and drop

---
+++ Gavriliyuk/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool selected = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(selected)
+        {
+            Vector2 mousePos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePos.x, mousePos.y);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            selected = false;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            selected = true;
+        }
+    }
+}
