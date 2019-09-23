c530a69ac8e3bf299bdb0ca3ad7c7e2bb5f0d55e
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-23 19:25:49 +0300

    tile behaviour

---
+++ Gavriliyuk/Assets/Scripts/FieldTile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+public class FieldTile : MonoBehaviour
+{
+    [SerializeField] float chanceToDigUp = 30f;
+    [SerializeField] List<GameObject> dropItems;
+    [SerializeField] Tilemap fieldTilemap;
+
+    private Vector3 previousPosition;
+    private int tileLevel = 0;
+
+    public event Action OnClick;
+
+    private void OnMouseDown()
+    {
+        OnClick();
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float strictMouseX = Mathf.RoundToInt(mousePos.x);
+        float strictMouseY = Mathf.RoundToInt(mousePos.y);
+        Vector2 newMousePos = new Vector2(strictMouseX, strictMouseY);
+
+        DigUp(newMousePos);
+
+        if (RandomInstantiation())
+        {
+            Instantiate(dropItems[RandomDrop()], newMousePos, transform.rotation);
+        }
+    }
+
+    private bool RandomInstantiation()
+    {
+        float probability = Random.Range(0, 100);
+
+        if(probability<=chanceToDigUp)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private int RandomDrop()
+    {
+        int rand = Random.Range(0, dropItems.Count);
+
+        return rand;
+    }
+
+    private void DigUp(Vector3 position)
+    {
+        Vector3Int currentCell = fieldTilemap.WorldToCell(position);
+
+        fieldTilemap.SetTile(currentCell, null);
+        Debug.Log("Delete");
+    }
+}
